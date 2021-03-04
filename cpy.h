f6267a5f2a86f5e4bccc80162bc27aade0d7e384
VukAleksandrov <nesabojana3@gmail.com>
2021-03-04 14:03:26 +0100

    Added rezize function.

--- cpy.h
+++ cpy.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 typedef unsigned int uint32_t;
 using namespace std;
 
@@ -11,57 +12,57 @@ namespace cpy
 	{
 	private:
 
-		uint32_t rowSize,
-			columnSize;
 
-		int* MATRIX = new MATRIX_TYPE[rowSize * columnSize];
-
+		int* MAIN_MATRIX = new MATRIX_TYPE[rowSize * columnSize];
 
 	public:
 
+		uint32_t rowSize,
+			columnSize;
+
 		matrix(uint32_t n, uint32_t m)
-			:rowSize(n), columnSize(m)
+			:rowSize(n), columnSize(m)	
 		{
 			for (int i = 0; i < size(); ++i)
-				MATRIX[i] = 0;
+				MAIN_MATRIX[i] = 0;
 		}
 
 		virtual ~matrix()
 		{
-			delete[] MATRIX;
+			delete[] MAIN_MATRIX;
 		}
 
 
-		//initialize index with brackets [][]
+	//  initialize index with brackets [][]
 		const int* operator [](int*& rowIndex) const
 		{
-			return &MATRIX[*rowIndex * columnSize];
+			return &MAIN_MATRIX[*rowIndex * columnSize];
 		}
 
 		const int* operator [](int& columnIndex) const
 		{
-			return &MATRIX[columnIndex * columnSize];
+			return &MAIN_MATRIX[columnIndex * columnSize];
 		}
 
 
 		matrix& operator= (matrix& revalueTo)
-			//can't revalue matrix with an array or heap,
-			//only with same type
+	//  can't revalue matrix with an array or heap,
+	//  only with same type
 		{
-			this->MATRIX = revalueTo.MATRIX;
+			this->MAIN_MATRIX = revalueTo.MAIN_MATRIX;
 
 			return *this;
 		}
 
 
 		const bool operator == (matrix& compareTo)
-			//works only on same-size matrices
+	//  works only on same-size matrices
 		{
 			bool areEqual = true;
 
 			for (int i = 0; i < rowSize * columnSize; ++i)
 			{
-				if (&MATRIX[i] != compareTo.MATRIX[i])
+				if (&MAIN_MATRIX[i] != compareTo.MAIN_MATRIX[i])
 				{
 					areEqual = false;
 					break;
@@ -81,7 +82,24 @@ namespace cpy
 			return rowSize * columnSize;
 		}
 
+		void resize(uint32_t newRowSize,uint32_t newColumnSize)
+	//  Values surounding matrix are valuated to zero
+		{
+			int ResizedMatrixValue[newRowSize][newColumnSize] = { 0 };
+
+			for (int i = 0; i < rowSize; i++) {
+
+				for (int j = 0; j < columnSize; j++)
+				{
+					ResizedMatrixValue[i][j] = MAIN_MATRIX[i * columnSize + j];
+				}
+			}
+
+			rowSize = newRowSize;
+			columnSize = newColumnSize;
+		}
 	
+
 		int* row(uint32_t row_)
 	 // you can take the row if the l-value you are 
 	 //	giving this row/column is an heap-allocated array 
@@ -89,7 +107,7 @@ namespace cpy
 			int* rowCopy= new int[&columnSize];
 			for (int i = row_; i < columnSize * row_; ++i)
 			{
-				rowCopy[i-row_] = &MATRIX[i];
+				rowCopy[i-row_] = &MAIN_MATRIX[i];
 				
 			}
 			return  rowCopy;
@@ -103,7 +121,7 @@ namespace cpy
 			int* columnCopy = new int[&rowSize];
 			for (int i = column_; i < rowSize * column_; ++i)
 			{
-				columnCopy[i - column_] = &MATRIX[i];
+				columnCopy[i - column_] = &MAIN_MATRIX[i];
 
 			}
 			return  columnCopy;
